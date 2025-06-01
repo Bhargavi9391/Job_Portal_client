22ab08ac9f4266a3c623dde1c17285a22670c42e
jaddu Bhargavi <157037629+Bhargavi9391@users.noreply.github.com>
2025-06-01 20:48:02 +0530

    Update Admin.js

--- src/Admin.js
+++ src/Admin.js
@@ -142,7 +142,7 @@ export default function Admin() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/jobs", {
+      const response = await fetch("https://job-portal-server-1-nt8w.onrender.com/api/jobs", {
         // Replace with your actual backend URL
         method: "POST",
         headers: {
